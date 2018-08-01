52fd02f608b7e3ecf49a63a26c2487b221ed1830
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-01 23:22:35 +0530

    5th program done

---
+++ CSPP1- Practice/m3/iterate_even_reverse.py
@@ -0,0 +1,15 @@
+'''
+author : SupriyaMadupathi
+ IN this Program we use while, For loop to get
+ output  .
+ '''
+print('Hello!')
+A = 10
+while A >= 2:
+    print(A)
+    A = A-2
+
+print('Hello!')
+for B in range(10, 0, -2):
+    print(B)
+    
