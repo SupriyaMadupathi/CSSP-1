8159bc9974c11a1fa246f681e39ac277a0fb0ce3
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-02 21:26:55 +0530

    " -> 0 of 5 passed. pylint: 10.00/10 "

---
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -0,0 +1,32 @@
+'''
+Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s.
+For example, if s = 'azcbobobegghakl', then your program should print
+
+Number of times bob occurs is: 2
+@ author SupriyaMadupathi
+'''
+
+def main():
+    '''
+    Assume s is a string of lower case characters.
+
+    Write a program that prints the number of times the string 'bob' occurs in s.
+    '''
+    str_input1 = input()
+    sub_input2 = input()
+    count = 0
+    flag = 1
+    start = 0
+    while flag:
+        a_i = str_input1.find(sub_input2, start)
+        if a_i == -1:
+            flag = 0
+        else:
+            count += 1
+            start = a_i+1
+    print(count)
+
+if __name__ == "__main__":
+    main()
