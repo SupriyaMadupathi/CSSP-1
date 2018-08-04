f2712761e368dbb240f4750a6ae5a4f6f7749925
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-04 11:08:07 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p1/fizz_buzz.py
+++ cspp1-assignments/m6/p1/fizz_buzz.py
@@ -10,13 +10,13 @@ def main():
     Read number from the input, store it in variable num.
     '''
     var = int(input())
-    for i in range(1, var, 1):
-        if i % 3 == 0:
+    for i in range(1, var+1, 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizz")
+            print("Buzz")
+        elif i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
-            print("Fizz")
-        elif i % 3 == 0 and  i % 5 == 0:
-            print("Fizz")
             print("Buzz")
         else:
             print(i)
