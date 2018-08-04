6babf51a5ff8f07812b604d512af431887962701
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-04 12:41:42 +0530

    " -> 4 of 7 passed. pylint: 9.00/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -1,7 +1,6 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
-    input: 123
+example: input: 123
     output: 6
     @ author SupriyaMadupathi
 '''
@@ -10,11 +9,11 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     i = int(input())
-    p = 1
+    produc_t = 1
     while i > 0:
-        a = i % 10
+        a_b = i % 10
         i = i // 10
-        p = p*a
-    print(p)
+        produc_t = produc_t*a_b
+    print(produc_t)
 if __name__ == "__main__":
     main()
