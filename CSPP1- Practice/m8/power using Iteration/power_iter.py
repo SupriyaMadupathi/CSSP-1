ef00169ee0eb4eed020e5756d4aeaadb3bb449c7
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-07 16:47:17 +0530

    iterative power

---
+++ CSPP1- Practice/m8/power using Iteration/power_iter.py
@@ -0,0 +1,35 @@
+'''
+# Exercise: PowerIter
+# Write a Python function, iterPower(base, exp), that takes in
+ two numbers and returns the base^(exp) of given base and exp.
+
+# This function takes in two numbers and returns one number.
+@ author : SupriyaMadupathi
+'''
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    result = 1
+    while exp > 0:
+        result = base * result
+        exp =exp - 1
+    return result
+
+
+def main():
+    '''
+    iterative Power
+    '''
+
+    data = input()
+    data = data.split()
+    print(iterPower(float(data[0]), int(data[1]))) 
+
+if __name__ == "__main__":
+    main()
