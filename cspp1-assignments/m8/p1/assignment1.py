70ea5d34c0872511e5f18361440d8e8eca315ac4
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-07 19:23:18 +0530

    " -> 9 of 10 passed. pylint: 4.44/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -18,7 +18,8 @@ def factorial(n):
     if n == 1 or n == 1:
         return 1
     else:
-        return (n * factorial(n-1)
+        return (n * factorial(n-1))
+
 def main():
     '''
     n is positive Integer
