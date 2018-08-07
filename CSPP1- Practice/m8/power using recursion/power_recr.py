51780d27a4e0a093f60df903346efc15ca06c1c5
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-07 17:09:19 +0530

    recursive power

---
+++ CSPP1- Practice/m8/power using recursion/power_recr.py
@@ -0,0 +1,35 @@
+'''
+# Exercise: PowerRecr
+# Write a Python function, recurPower(base, exp), that takes in two numbers 
+and returns the base^(exp) of given base and exp.
+
+# This function takes in two numbers and returns one number.
+@ author SupriyaMadupathi
+'''
+
+
+def recurpower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    if exp == 1:
+        return base
+    elif exp == 0:
+        return 1
+    else:
+        return base * recurpower(base, exp-1)
+
+def main():
+    '''
+    Recursive PowerRecr
+    '''
+    data = input()
+    data = data.split()
+    print(recurpower(float(data[0]), int(data[1])))
+
+if __name__ == "__main__":
+    main()
