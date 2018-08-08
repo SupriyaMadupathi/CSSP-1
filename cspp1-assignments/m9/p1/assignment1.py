1171a5bb88fe83731e188ee08ed3ea8f8d74d5af
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-08 17:29:24 +0530

    " -> 5 of 9 passed. pylint: 10.00/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -17,7 +17,7 @@ def is_word_guessed(secret_word, letters_guessed):
     '''
     # FILL IN YOUR CODE HERE...
     for i in secret_word:
-        if i in Letters_guessed:
+        if i in letters_guessed:
             return True
     return False
 
