f2e18274c3c9612f4dd98fab779590f1500facff
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-08 18:20:05 +0530

    " -> 0 of 9 passed. pylint: 8.95/10 "

--- cspp1-assignments/m9/p2/assignment2.py
+++ cspp1-assignments/m9/p2/assignment2.py
@@ -14,6 +14,7 @@ def get_guessed_word(secret_word, letters_guessed):
       what letters in secret_word have been guessed so far.
     '''
     # FILL IN YOUR CODE HERE...
+    s_a = ' '
     for i in secret_word:
         if i not in letters_guessed:
             return '_'
