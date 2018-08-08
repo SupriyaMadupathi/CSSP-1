f823bf85fbfe3b410cdc18241642b8e39a969403
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-08 16:48:23 +0530

    increment by 1

---
+++ CSPP1- Practice/m9/Functions and Objects Exercise-2/functions_and_objects_2.py
@@ -0,0 +1,31 @@
+'''
+#Exercise : Function and Objects Exercise-2
+#Implement a function that converts the given 
+testList = [1, -4, 8, -9] into [2, -3, 9, -8]
+@ author SupriyaMadupathi
+'''
+def inc(i):
+    return i+1
+
+def apply_to_each(L, f):
+   '''
+   increment in value by 1
+   '''
+   for i in range(len(L)):
+        L[i] = f(L[i])
+   print (L)
+
+def main():
+    '''
+    increment in value by 1
+    '''
+
+    data = input()
+    data = data.split()
+    list1 = []
+    for j in data:
+        list1.append(int(j))
+    apply_to_each(list1, inc)
+
+if __name__ == "__main__":
+    main()
