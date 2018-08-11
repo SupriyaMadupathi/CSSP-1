dc3567dd22047b5eafbc890f7ec582abdca37d64
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-11 16:39:49 +0530

    " -> 6 of 6 passed. pylint: 8.33/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -28,18 +28,3 @@ def main():
 if __name__ == "__main__":
     main()
     
-
-def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
-
-
-if __name__== "__main__":
-	main()
