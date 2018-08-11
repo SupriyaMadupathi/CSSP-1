ac26e5468a37726e946547884b974a94f11ca10c
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-11 16:37:56 +0530

    " -> 0 of 6 passed. pylint: -2.76/10 "

---
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -0,0 +1,45 @@
+'''
+#Exercise: Assignment-2
+#Implement the updateHand function. Make sure this function has 
+no side effects: i.e., it must not mutate the hand passed in. 
+Before pasting your function definition here, be sure you've 
+passed the appropriate tests in test_ps4a.py.
+@ author: SupriyaMadupathi
+'''
+
+def update_hand(hand_1, word_1):
+    """a2"""
+    hand_new = dict(hand_1)
+    for i in word_1:
+        if i in hand_1.keys():
+            hand_new[i] = hand_new[i] - 1
+    return hand_new
+def main():
+    """string"""
+    n_1 = input()
+    adict_1 = {}
+    for i in range(int(n_1)):
+        data = input()
+        i = i
+        l_1 = data.split(" ")
+        adict_1[l_1[0]] = int(l_1[1])
+    data1 = input()
+    print(update_hand(adict_1, data1))
+if __name__ == "__main__":
+    main()
+    
+
+def main():
+	n=input()
+	adict={}
+	for i in range(int(n)):
+		data=input()
+		l=data.split()
+		adict[l[0]]=int(l[1])
+	data1=input()
+	print(updateHand(adict,data1))
+		
+
+
+if __name__== "__main__":
+	main()
