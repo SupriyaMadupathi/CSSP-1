88c4b4442d24ad00910c164e112808e613a80bcf
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-11 16:15:54 +0530

    " -> 9 of 9 passed. pylint: 2.78/10 "

--- cspp1-assignments/m12/p2/social_network.py
+++ cspp1-assignments/m12/p2/social_network.py
@@ -14,7 +14,7 @@ def follow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    pass
+    
 
 def unfollow(network, arg1, arg2):
     '''
@@ -26,7 +26,6 @@ def unfollow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    pass
 
 def delete_person(network, arg1):
     '''
