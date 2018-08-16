dcb2690b9d91648e99e96ba554828f7fad6743aa
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-16 18:50:25 +0530

    " -> 7 of 8 passed. pylint: 9.67/10 "

--- cspp1-assignments/m15/poker.py
+++ cspp1-assignments/m15/poker.py
@@ -17,7 +17,7 @@ def suit_values(hand):
     '''
     suit values
     '''
-    suit_values = [suit_value for face, suit_value in hand]
+    suit_values = [suit_val for face, suit_val in hand]
     return suit_values
 def is_four_of_kind(hand):
     '''
