6e75f034f62462865bd4184a3c89495e730e772d
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-24 19:29:56 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- cspp1-assignments/m21/tictactoe.py
+++ cspp1-assignments/m21/tictactoe.py
@@ -10,8 +10,26 @@ def input_validation(matrix):
 			if j not in "x.o":
 				return False
 	return True
-def start_game(matrix):
-	pass
+def start_game(matrix, winner_variable):
+	for row in matrix:
+		if len(set(row)) == 1 and row[0] == winner_variable:
+			return True
+	return False
+
+def new_transpose(matrix, increment, temp_matrix = []):
+	if increment == len(matrix):
+		return temp_matrix
+	else:
+		temp_matrix.append(matri[0][increment], matrix[1][increment], matrix[2][increment])
+		return new_transpose(matrix, increment+1, temp_matrix[])
+def decide_winner(matrix, winner_variable):
+	transpose_matrix = new_transpose(matrix, 0, [])
+	if start_game(matrix, winner_variable) or\
+	start_game(transpose_matrix, winner_variable) or\
+	matrix[0][0] == matrix[1][1] == matrix[2][2] == winner_variable:
+		return True
+	return False
+	
 def invalid_game(matrix):
 	count_x = 0
 	count_o = 0
@@ -22,15 +40,18 @@ def invalid_game(matrix):
 		return False
 	return True
 
-def main():
-	matrix = tic_tac_input()
+def main(matrix):
+	#matrix = tic_tac_input()
 	#print(input_validation(matrix))
-	if not input_validation(matrix):
-		print("invalid input")
-	elif not invalid_game(matrix):
-		print("invalid game")
+	if input_validation(matrix):
+		if invalid_game(matrix):
+			if decide_winner(matrix, 'x'):
+				print('x')
+			elif decide_winner(matrix, 'o'):
+				print('o')
+			else:
+				print('draw')
 	else:
-		start_game(matrix)
-
+		print("invalid input")
 if __name__ == '__main__':
     main()
