612afa336642a015dc567ebb8e80fb3c77483f7b
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-12-21 11:08:28 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -16/10 "

--- cspp1-assignments/cspp1 exam/CSPP-1_Question/Sudoku_solver.py
+++ cspp1-assignments/cspp1 exam/CSPP-1_Question/Sudoku_solver.py
@@ -2,7 +2,6 @@ grid = []
 
 def assign(values, s, d):
     other_values = values[s].replace(d, '')
-    print("Given sudoku is solved")
     if all(eliminate(values, s, d2) for d2 in other_values):
         return values
     else:
