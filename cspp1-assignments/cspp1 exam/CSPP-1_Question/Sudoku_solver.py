a0fa58a50a87d32381d52fbcd9bfed786ef6d5a7
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-12-21 12:15:09 +0530

    "cspp1_m10_a1_1: 9/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- cspp1-assignments/cspp1 exam/CSPP-1_Question/Sudoku_solver.py
+++ cspp1-assignments/cspp1 exam/CSPP-1_Question/Sudoku_solver.py
@@ -1,16 +1,16 @@
 """
 { Sudoku_solver}
 """
-Sudoku_Solver = input()
+Sudoku_solver = input()
 grid = [True]*9
-if '.' not in Sudoku_Solver:
+if '.' not in Sudoku_solver:
     print("Given sudoku is solved")
 elif len(Sudoku_solver) != 81:
     print("Invalid input")
 else:
     count = 0
     i = ""
-    for i in Sudoku_Solver:
+    for i in Sudoku_solver:
         if count == 9:
             for j in range(9):
                 if grid[j] == True:
