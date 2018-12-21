ee523e34f881f064b7ecd8cb618f3934b6ed0124
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-12-21 10:19:55 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-assignments/cspp1 exam/CSPP-1_Question/Sudoku_solver.py
@@ -0,0 +1,22 @@
+def assign(values, s, d):
+    other_values = values[s].replace(d, '')
+    if all(eliminate(values, s, d2) for d2 in other_values):
+        return values
+    else:
+        return False
+#def display(values):
+def search(values):
+    
+        return False 
+        if all(len(values[s]) == 1 for s in squares):
+            return values
+        n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+        return some(search(assign(values.copy(), s, d))
+                for d in values[s])
+def sudoku(arr):
+    inp = int(input()).split()
+    print(inp)
+    for i in range(9):
+        for j in range(9):
+            print(arr[i][j])
+        print("n")
