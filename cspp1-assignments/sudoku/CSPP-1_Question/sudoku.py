3ff25db615bbcf22ee6a315c3bccb96be61aaf50
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-12-25 10:26:52 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -10/10 "

---
+++ cspp1-assignments/sudoku/CSPP-1_Question/sudoku.py
@@ -0,0 +1,46 @@
+"""
+In this method :
+ * Check there are only 81 values
+ * iterate through each row in the sudoku and if you find any duplicate values
+ 	raise an exception
+ * iterate through each column in the sudoku and if you find any duplicate values
+	raise an exception
+ * iterate through each subgrid(3x3) in the sudoku and if you find any duplicate values
+	raise an exception
+"""
+def validateSudoku(sudoku):
+	pass
+
+"""
+This  method should retunn all the values present in the ith row
+"""
+def getRowValues():
+	pass
+"""
+This  method should retunn all the values present in the ith column
+"""
+def getColumnValues():
+	pass
+
+"""
+This  method should retunn all the values present in the i,j th subgrid
+"""
+def getGridValues():
+	pass
+"""
+This method should collect all the available values present for a "."
+You should get the values present in row,column,grid.
+Then you should return the values that doesnot exist in the previous values.
+"""
+def possibleValues():
+	pass
+"""
+Read the input and store the values in an appropriate data sturcture.
+Then travese through each value, if you get a "." then collect the possible values
+"""
+def main():
+	inpu = input()
+	if len(inpu) != 81:
+		print("Invalid Input")
+	elif "." not in inpu:
+		print("Given sudoku is solved")
---
+++ cspp1-assignments/sudoku/CSPP-1_Question/sudoku_solver.py
@@ -0,0 +1,14 @@
+sudoku = input()
+if len(sudoku) != 81:
+ 	print("Invalid Input")
+elif '.' not in sudoku:
+ 	print("Sudoku solved")
+else:
+ 	grid = list(sudoku)
+ 	list1 = []
+ 	list2 = []
+ 	count = 0
+ 	for i in range(len(list1)):
+ 		count[i] += 1
+ 		if count[1] == 2:
+ 			print("Invalid Sudoku:Duplicate values")
