84457b89d31c6d99c6d08023d62eb9f3361a3386
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-12-25 11:02:55 +0530

    "cspp1_m10_a1_1: 3/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 2/10 "

--- cspp1-assignments/sudoku/CSPP-1_Question/sudoku_solver.py
+++ cspp1-assignments/sudoku/CSPP-1_Question/sudoku_solver.py
@@ -1,19 +1,37 @@
 sudoku = input()
 if len(sudoku) != 81:
- 	print("Invalid input")
+    print("Invalid input")
 elif '.' not in sudoku:
- 	print("Given sudoku is solved")
+    print("Given sudoku is solved")
 else:
- 	grid = list(sudoku)
- 	list1 = []
- 	list2 = []
- 	count = 0
- 	#print(grid)
- 	for i in range(len(grid)):
- 		#print(grid)
- 		list1.append(grid[i])
- 		print(list1)
- 		#for i in list1:
- 		#	count[i] += 1
- 		#	if count[1] == 2:
- 		#		print("Invalid Sudoku:Duplicate values")
+    i = ""
+    grid = list(sudoku)
+    list1 = []
+    list2 = []
+    count = 0
+    for i in Sudoku_solver:
+        if count == 9:
+            for j in range(9):
+                if grid[j] == True:
+                    print(j+1)
+            grid = [True]*9
+            count = 0
+        count = count + 1
+        if i == '.':
+            continue
+        grid[int(i) - 1] = False
+    if count == 9:
+            for j in range(9):
+                if grid[j] == True:
+                    print(j+1)
+            grid = [True]*9
+            count = 0
+    #print(grid)
+    #for i in range(len(grid)):
+        #print(grid)
+    #   list1.append(grid[i])
+    #   print(list1)
+        #for i in list1:
+        #   count[i] += 1
+        #   if count[1] == 2:
+        #       print("Invalid Sudoku:Duplicate values")
