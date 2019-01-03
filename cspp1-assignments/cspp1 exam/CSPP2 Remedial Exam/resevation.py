35af3d858240fc0abe4de6d530aff43641ad1ee0
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-03 15:22:02 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-assignments/cspp1 exam/CSPP2 Remedial Exam/resevation.py
@@ -0,0 +1,62 @@
+def main():
+    data = int(input())
+    dic = {}
+    i = 0
+    capacity = 6
+    # count = 1
+    while i < data:
+        # print(i)
+        data1 = input().split(" ")
+        # print(data1[0])
+        if data1[0] == "reserveN":
+            if len(dic) >= capacity:
+                print("All Rooms are reserved")
+            elif int(data1[2]) in dic.keys():
+                print("Room is already reserved")
+            else:
+        dic[int(data1[2])] = data1[1]
+                print(data1[1], data1[2])
+
+            # count += 1
+    # print(dic)s
+        # print(dic)
+        if data1[0] == "reserve":
+            if dic == {}:
+                dic[1] = data1[1]
+                # count += 1
+            # if count > 5:
+                            # print(len(d))
+                # print("All Rooms are reserved")
+                # break
+            else:
+                for  j in range(1, 7):
+                    # print(j)
+                    
+                    if  j not in dic.keys():
+                        # print(j, "j")
+                        # print(capacity, "capacity")
+                        if j >= int(capacity):
+                        # print(len(d))
+                            print("All Rooms are reserved")
+                            break
+
+                        dic[j] = data1[1]
+                        # count += 1
+                        break
+                    # print(len(data1))
+                # print(count)
+            # count += 1
+            # print(count)
+        i += 1
+    for a in dic:
+        print(dic[a], a)
+    # print(dic)
+    for x in sorted(dic):
+        print(dic[x], x)
+    # sort = sorted(dic)
+    # print(sort)
+
+        
+
+if __name__ == '__main__':
+    main()
