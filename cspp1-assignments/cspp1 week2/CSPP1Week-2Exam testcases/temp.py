cfdf7fb74aa33ff21e20d27074ea43dadfeca9c3
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-05 12:35:56 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/10 "

---
+++ cspp1-assignments/cspp1 week2/CSPP1Week-2Exam testcases/temp.py
@@ -0,0 +1,6 @@
+x = "zsxdfcgvbhjnkkhhk"
+c =0 
+for each in x:
+	if "k" in each:
+		c += 1
+print(c)
--- cspp1-assignments/cspp1 week2/CSPP1Week-2Exam testcases/week2.py
+++ cspp1-assignments/cspp1 week2/CSPP1Week-2Exam testcases/week2.py
@@ -1,22 +1,40 @@
+def sum(numList):
+    theSum = 0
+    for i in numList:
+        theSum = theSum + i
+    return theSum
 def main():
-	data = eval(input())
-	summ = 0
-	# li = []
-	x = ""
-	# li.append(data)
-	for each in data:
-		if x != type(each):
-			summ += each
-		# for i1 in each:
-		# 	# for i2 in i1:
-		# 	li.append(i1)
-		# 	# li.append(i1)
-		# 	for x in li:
-		# 		if y != type(x):
-		# 			summ += x
+    data = eval(input())
+    sum(data)
+    # summ = 0
+    # li = []
+    # x =list(sum(data, []))
+    # print(x)
+    # li.append(data)
+    # for each in data:
+    #     #
+    #     for i1 in each:
+    #         x =""
+    #     #   # for i2 in i1:
+    #         li.append(i1)
+    #         for a in li:
+    #             if type(x) != type(each):
+    #                 summ += each
+    #     #   # li.append(i1)
+        #   for x in li:
+        #       if y != type(x):
+        #           summ += x
 
-	print(summ)
-			
+    # print(summ)
+    # count = 0
+    # for each in data:
+    #     if list in each:
+    #         print("hi")
+    #         count += 1
+        # print(count)
+
+
+            
 
 if __name__ == '__main__':
-	main()
+    main()
