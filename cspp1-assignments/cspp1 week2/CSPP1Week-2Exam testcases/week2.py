8c76fa84d23b54fd53a9847372b9d35c1897f9cd
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-05 10:23:09 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- cspp1-assignments/cspp1 week2/CSPP1Week-2Exam testcases/week2.py
+++ cspp1-assignments/cspp1 week2/CSPP1Week-2Exam testcases/week2.py
@@ -1,14 +1,18 @@
 def main():
 	data = eval(input())
 	summ = 0
+	count = 0
 	# li = []
 	# li.append(data)
 	for each in range(len(data)):
 		# if type(i) == list:
 			summ += each
+			count += 1
+			avg = summ/count
 		# else:
 			# summ += i
-			print(summ)
+	# print(summ)
+	print(avg)
 	# for each in data:
 	# 	summ += int(each) 
 	# 	print(summ)
