82de74e989320d74b4ed63138c52af22978ead2a
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-10 17:36:14 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- cspp1-assignments/cspp1 week2/Assignment-1/quiz.py
+++ cspp1-assignments/cspp1 week2/Assignment-1/quiz.py
@@ -7,9 +7,9 @@ def loadquestions(data):
     # print(inp)
     for i in range(0, inp):
         li.append(input().split(":"))
-    if len(li) < 5:
-        print("Error! Malformed question")
-    elif inp >= 1:
+    # if len(li) < 5:
+        # print("Error! Malformed question")
+    # elif inp >= 1:`
         print(inp, "are added to the quiz")
         # print("Error! Malformed question")
     else:
