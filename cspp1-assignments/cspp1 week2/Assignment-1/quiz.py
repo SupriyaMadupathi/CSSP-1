7073c03628431e7c9baa107cb9c2072aabaf1ca9
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-10 11:00:15 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

--- cspp1-assignments/cspp1 week2/Assignment-1/quiz.py
+++ cspp1-assignments/cspp1 week2/Assignment-1/quiz.py
@@ -1,46 +1,70 @@
+li = []
 def loadquestions(data):
-	li = []
-	print("Load Questions")
-	inp = int(data[1])
-	# print(inp)
-	for i in range(0, inp):
-		li.append(input().split(":"))
-	if inp > 1:
-		print(inp, "are added to the quiz")
-	else:
-		print("no questions are to be added")
+    print("Load Questions")
+    inp = int(data[1])
+    # print(inp)
+    for i in range(0, inp):
+        li.append(input().split(":"))
+    if inp > 1:
+        print(inp, "are added to the quiz")
+    else:
+        print("no questions are to be added")
 
 def startquiz(data):
-	li = []
-	print("Start Quiz")
-	inp1 = int(data[1])
-	print(inp1)
+    # li = []
+    print("Start Quiz")
+    # print("hi")
+    inp1 = int(data[1])
+    for x in li:
+        # print("hooo")
+        ans = input().split(" ")
+        x.append(ans[1])
+        print(x[0]+"("+ x[3]+")")
+        choice = x[1].split(",")
+        print(choice[0],"   ",choice[1],"   ",choice[2],"   ",choice[3])
 
+    # print(inp1)
+def score(data):
+    summ = 0
+    for i in li:
+        print(i[0])
+        if i[2]== i[5]:
+            print("Correct Answer! - Marks Awarded:", i[3])
+            summ = summ+int(i[3])
+            # print(summ)
+        else:
+            print(" Wrong Answer! - Penalty: -4", i[4])
+            summ = summ + int(i[4])
+    print("Total Score: ", summ)
+
+
+    pass
 
 def main():
-	while True:
-		data = input()
-		if data:
-			data = input().split()
-			if data[0] == "LOAD_QUESTIONS":
-				loadquestions(data)
-			if data[0] == "START_QUIZ":
-				startquiz(data)
-			if data[0] == "SCORE_REPORT":
-				score(data)
-		else :
-			break
-	# q = int(data[1])
-	# loadquestions(data)
-	# print(type(q))
-	# print(data)
-	# if data[0] == "LOAD_QUESTIONS":
-		# print("|----------------|")
-		# print("| Load Questions |")
-		# print("|----------------|")
+        while True:
+            try:
+                # data = input()
+                # if data:
+                data = input().split()
+                if data[0] == "LOAD_QUESTIONS":
+                    loadquestions(data)
+                if data[0] == "START_QUIZ":
+                    startquiz(data)
+                if data[0] == "SCORE_REPORT":
+                    score(data)
+            except EOFError:
+                break
+    # q = int(data[1])
+    # loadquestions(data)
+    # print(type(q))
+    # print(data)
+    # if data[0] == "LOAD_QUESTIONS":
+        # print("|----------------|")
+        # print("| Load Questions |")
+        # print("|----------------|")
 
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
