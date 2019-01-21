f8370ce2f7bf08500918dbb63404541761708f5b
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-21 14:21:51 +0530

    "cspp1_m10_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -1/10 "

--- cspp1-assignments/cspp1 wee3/new/CSPP1-Week3Exam/quiz_report.py
+++ cspp1-assignments/cspp1 wee3/new/CSPP1-Week3Exam/quiz_report.py
@@ -1,61 +1,122 @@
+from collections import OrderedDict
+def checklength(adict):
+	maxno = 0
+	maxno = max(len(x) for x in adict)
+	for each in adict:
+		if len(each) != maxno:
+			return False
+	return True
+
+def display(adict, adict1):
+	score = 0
+	if checklength(adict):
+		for key1 in sorted(adict):
+			for key2 in sorted(adict1):
+				if key1 == key2:
+					# print(adict[key1], adict1[key2])
+					score = int((adict[key1]/adict1[key2])*100)
+					if score < 0:
+						score = 0
+					print(key1 + ": " + str(float(score)) + "%")
+	else:
+		s = sorted(adict.items(),key=lambda x: (len(x[0]), x))
+		s1 = sorted(adict1.items(),key=lambda x: (len(x[0]), x))
+		# print(adict)
+		# print(adict1)
+		# print("-------")
+		orddict2 = listtodict(s)
+		orddict3 = listtodict(s1)
+		print(orddict2)
+		print(orddict3)
+		for key1 in orddict2:
+			for key2 in orddict3:
+				if key1 == key2:
+					# print(adict[key1], adict1[key2])
+					score = int((orddict2[key1]/orddict3[key2])*100)
+					if score < 0:
+						score = 0
+					print(key1 + ": " + str(float(score)) + "%")	
+
+def listtodict(s):
+	orddict1 = {}
+	for each in s:
+		# print(each[0], each[1])
+		orddict1[each[0]] = each[1]
+	# print(orddict1)
+	return orddict1
+def displaynew(adict, adict1):
+	adictx = {}
+	for each in sorted(adict):
+		for eachx in sorted(adict1):
+			total = 0
+			if each == eachx:
+				adictx = adict[each]
+				print(adictx)
+				for each1 in adictx:
+					total += adictx[each1]
+				score = total/adict1[eachx]
+				print(eachx)
+				print(total)
+				print(score)
 
 def main():
-	data = int(input())
+		adict = {}
+		adict1 = {}
+		ques = {}
+		n = int(input())
+		try:
+			for i in range(n):
+				string = input().split("|")
+				int(string[4])
+				if string[0] not in adict:
+					ques = {}
+					adict[string[0]] = 0
+					adict1[string[0]] = int(string[4])
+				else:
+					adict1[string[0]] += int(string[4])
+				if string[2] == string[3]:
+					adict[string[0]] += int(string[4])
+				else:
+					adict[string[0]] -= int(string[4])
+			# print(adict)
+			# print(adict1)
+			display(adict, adict1)
+		except ValueError:
+			print("Invalid Points")
+		# try:
+		# 	for i in range(n):
+		# 		string = input().split("|")
+		# 		int(string[4])
+		# 		if string[0] not in adict:
+		# 			ques = {}
+		# 			if string[2] == string[3]:
+		# 				ques[string[1]] = int(string[4])
+		# 				adict[string[0]] = ques
+		# 				adict1[string[0]] = int(string[4])
+		# 			else:
+		# 				ques[string[1]] = "-" + string[4]
+		# 				adict[string[0]] = ques
+		# 				adict1[string[0]] = int(string[4])
 
-	# print(data)
-	li = []
-	l2 = []
-	total = 0
-	summ = 0
-	for i in range(data):
-		response = input().split("|")
-		li.append(response)
-		# li.groupby(x[0], x[4])
-		# for each in li:
-			# if each[]
-		# for each in range(len(response)):
-			# if int(each[i]) == int(li[i]):
-				# l2.append(i)
-			# li.append(each)
-			# dic[ea]  = li
-
-	print(li)
-	dic = []
-	for x in li:
-		if x[2] == x[3]:
-			dic.append(x[4])
-			# dic[x[0]] = x[4]
-			# break
-	print(dic)
-	for each in dic:
-		summ += int(each)
-	print(summ)
-		# for x in li:
-			# if x[i] == x[i+1]:
-			# l2 = {}
-			# dic[x[0]] = x[4]	
-			# print(dic)
-			# print(x)
-			# if x[2] == x[3]:
-				# summ += int(x[4])
-	# print(x[0],":" ,summ)
-		# print(summ)
-				# l2[x[0]] = x[4]
-				# l2.append(x[4])
-				# for y in l2:
-		# print(l2)
-
-			# dic ={}
-			# dic[x] = li
-			# print(dic)
-			# print(x)
-			# if x[2] == x[3]:
-				# li.append(x[4])
-				# print(li)
-
-		# print(response)
+		# 			# print(adict)
+		# 		else:
+		# 			adict1[string[0]] += int(string[4])
+		# 			for each in adict:
+		# 				if each == string[0]:
+		# 					if string[2] == string[3]:
+		# 						ques = adict[string[0]]
+		# 						ques[string[1]] = int(string[4])
+		# 					else:
+		# 						ques = adict[string[0]]
+		# 						ques[string[1]] = "-" + string[4]
 
 
-if __name__ == '__main__':
-	main()
+
+			# print(adict)
+			# print(adict1)
+			# displaynew(adict, adict1)
+
+		except ValueError:
+			print("Invalid Points")
+main()
 
