bb0e3d13ab75321998f0aaf7cd21d23ebefca41a
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-24 18:13:37 +0530

    "cspp1_m10_a1_1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 7/10 "

--- cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/solution.py
+++ cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/solution.py
@@ -7,6 +7,7 @@ def main():
         data = input().split("|")
         if data[4] not in "123456789":
             print("Invalid Points")
+            break
 
         else:
             if data[0] not in d:
