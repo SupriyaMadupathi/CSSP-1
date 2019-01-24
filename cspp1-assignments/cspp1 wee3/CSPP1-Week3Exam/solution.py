83f2960ae02219b76ed38a0b8b3c1cf69f166d3c
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-24 23:59:49 +0530

    "cspp1_m10_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

--- cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/solution.py
+++ cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/solution.py
@@ -5,20 +5,20 @@ def main():
     d2 = {}
     for i in range(n):
         data = input().split("|")
-        if data[4] not in "0123456789":
-            print("Invalid Points")
-            break
+        # if data[4] not in "0123456789":
+        #     print("Invalid Points")
+        #     break
 
+        # else:
+        if data[0] not in d:
+            d[data[0]] = int(data[4])
+            d2[data[0]] = 0
         else:
-            if data[0] not in d:
-                d[data[0]] = int(data[4])
-                d2[data[0]] = 0
-            else:
-                d[data[0]] += int(data[4])
-            if data[2] == data[3]:
-                d2[data[0]] += int(data[4])
-            else:
-                d2[data[0]] -= int(data[4])
+            d[data[0]] += int(data[4])
+        if data[2] == data[3]:
+            d2[data[0]] += int(data[4])
+        else:
+            d2[data[0]] -= int(data[4])
 
     for id1 in sorted(d):
         for id2 in d2:
