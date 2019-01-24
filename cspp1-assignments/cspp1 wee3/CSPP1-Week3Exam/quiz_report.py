e33d6ebf2d4b1fe941c6b8637eb1150705d7d032
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-24 10:53:21 +0530

    "cspp1_m10_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

---
+++ cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/quiz_report.py
@@ -0,0 +1,47 @@
+def check_response(data):
+    total_score = 0
+    scored = {}
+    global flag
+    flag = 0
+    full_Score = {}
+    for item in data:
+        try:
+            if int(item[4]) == type(int):
+                print("OKOK")
+        except ValueError:
+            print("Invalid Points")
+            flag = 1
+            break
+        if item[0] not in scored:
+                full_Score[item[0]] = int(item[4])
+                scored[item[0]] = 0
+        else:
+            full_Score[item[0]] += int(item[4])
+        if item[2] == item[3]:
+            scored[item[0]] += int(item[4])
+        else:
+            scored[item[0]] -= int(item[4])
+
+    for got in sorted(scored):
+        for total in sorted(full_Score):
+            if flag == 0:
+                if got == total:
+                    final = int((scored[got]/full_Score[total]) * 100)
+                    if final <= 0:
+                        final = 0.0
+                        print(got + ": " + str(final) + "%")
+                    else:
+                        print(got + ": " + str(float(final)) + "%")
+
+
+
+def main():
+    number = int(input())
+    global questions
+    questions = []
+    for i in range(number):
+        questions.append(input().split("|"))
+    check_response(questions)
+
+if __name__ == '__main__':
+    main()
