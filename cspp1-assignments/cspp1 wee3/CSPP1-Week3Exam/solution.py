738a3b09c267773419909f3a543cdae6de1b5036
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-01-24 18:07:49 +0530

    "cspp1_m10_a1_1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 7/10 "

--- cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/solution.py
+++ cspp1-assignments/cspp1 wee3/CSPP1-Week3Exam/solution.py
@@ -1,36 +1,37 @@
 def main():
-	n = int(input())
-	# print(n)
-	d = {}
-	d2 = {}
-	for i in range(n):
-		data = input().split("|")
-		if data[4] not in "123456789":
-			print("Invalid Points")
-		break
-		if data[0] not in d:
-			d[data[0]] = int(data[4])
-			d2[data[0]] = 0
-		else:
-			d[data[0]] += int(data[4])
-		if data[2] == data[3]:
-			d2[data[0]] += int(data[4])
-		else:
-			d2[data[0]] -= int(data[4])
-	# print(d)
-	# for x in range(n):
-	# 	if data[0] not in d2:
-	# print(d2)
-	for id1 in sorted(d):
-		for id2 in d2:
-			# print(type(id1))
-			if id1 == id2:
-				result = int((int(d2[id1])/ int(d[id2]))*100)
-				if result < 0:
-					result = 0.0
-		print(id1+": "+str(float(result))+"%" )
+    n = int(input())
+    # print(n)
+    d = {}
+    d2 = {}
+    for i in range(n):
+        data = input().split("|")
+        if data[4] not in "123456789":
+            print("Invalid Points")
+
+        else:
+            if data[0] not in d:
+                d[data[0]] = int(data[4])
+                d2[data[0]] = 0
+            else:
+                d[data[0]] += int(data[4])
+            if data[2] == data[3]:
+                d2[data[0]] += int(data[4])
+            else:
+                d2[data[0]] -= int(data[4])
+    # print(d)
+    # for x in range(n):
+    #   if data[0] not in d2:
+    # print(d2)
+    for id1 in sorted(d):
+        for id2 in d2:
+            # print(type(id1))
+            if id1 == id2:
+                result = int((int(d2[id1])/ int(d[id2]))*100)
+                if result < 0:
+                    result = 0.0
+        print(id1+": "+str(float(result))+"%" )
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
